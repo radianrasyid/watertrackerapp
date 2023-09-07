f1749dcab53b13f372ee20b85fc41b07091b1de5
radian rasyid <radian.rasyid9@gmail.com>
2023-09-07 13:47:24 +0700

    [radian] pushing new InputDecoration

--- lib/components/InputDecoration/profile_screen_input_decoration.dart
+++ lib/components/InputDecoration/profile_screen_input_decoration.dart
@@ -16,4 +16,19 @@ class ProfileScreenInputDecoration {
     filled: true,
     contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
   );
+
+  static InputDecoration profileScreenEditInputDecoration = InputDecoration(
+    fillColor: AppColor.mainBg.withOpacity(0.3),
+    enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: Colors.transparent,
+        ),
+        borderRadius: BorderRadius.circular(8)),
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(16),
+      borderSide: BorderSide(color: Colors.transparent),
+    ),
+    filled: true,
+    contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+  );
 }
